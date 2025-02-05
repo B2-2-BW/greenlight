24c66acc04668ab40b7d62c041c54d5667ebc2b2
이승민 <sodlqj5166@daum.net>
2025-02-05 22:29:29 +0900

    대기표 발급 기능 구현 1차
    register관련 패키지 생성 및 기초 api 개발
    
    Refs: #13
    Related: #2

---
+++ domain/src/main/java/com/winten/greenlight/domain/register/RegisterRepository.java
@@ -0,0 +1,9 @@
+package com.winten.greenlight.domain.register;
+
+import com.winten.greenlight.domain.Customer;
+import reactor.core.publisher.Mono;
+
+public interface RegisterRepository {
+    /* 사용자의 입장 티켓을 만들어주는 메서드(고유ID, 순번) */
+    Mono<Customer> generateTicket();
+}
