394b82700b9a46672167bc89f278991c08c54d6c
이승민 <sodlqj5166@daum.net>
2025-02-09 23:16:51 +0900

    티켓 저장 1차
    티켓 저장 관련 구현 및 레디스 저장기능 생성
    
    Refs: #13
    Related: #2

--- storage/db-core/src/main/java/com/winten/greenlight/db/domain/register/RegisterRepositoryImpl.java
+++ storage/db-core/src/main/java/com/winten/greenlight/db/domain/register/RegisterRepositoryImpl.java
@@ -2,10 +2,10 @@ package com.winten.greenlight.db.domain.register;
 
 import com.github.f4b6a3.tsid.TsidCreator;
 import com.winten.greenlight.domain.Customer;
-import com.winten.greenlight.domain.WaitingStatus;
 import com.winten.greenlight.domain.register.RegisterRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
@@ -13,6 +13,7 @@ import reactor.core.publisher.Mono;
 @Repository
 @RequiredArgsConstructor
 public class RegisterRepositoryImpl implements RegisterRepository {
+    private final ReactiveRedisTemplate<String, String> redisTemplate;
 
     @Override
     public Mono<Customer> generateTicket() {    //Entity 충돌방지를 위해 domain의 Object 임시로 사용
@@ -21,6 +22,16 @@ public class RegisterRepositoryImpl implements RegisterRepository {
             double waitingScore = System.currentTimeMillis(); // 대기 점수
 
             return new Customer(null, customerId, waitingScore, null);
-        });
+        }).doOnSuccess(ticket -> log.info("Generated ticket: {}", ticket));
+    }
+
+    @Override
+    public Mono<Customer> saveTicket(Customer customer) {
+        return Mono.just(new TempZSetEntity(customer))
+            .doOnNext(entity -> log.info("Saving: {}", entity))
+            .flatMap(entity -> redisTemplate.opsForZSet().add(entity.key(), entity.value(), entity.score())
+                .thenReturn(customer))
+            .doOnSuccess(saved -> log.info("Successfully saved ticket: {}", saved))
+            .doOnError(e -> log.error("Failed to save ticket", e));
     }
 }
