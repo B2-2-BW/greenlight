4e555f6dde4a851a6d45a32843398a65388dd247
gimminjeong <lovelyminjeong91@gmail.com>
2025-02-21 16:16:32 +0900

    3-check_queue_number 계산식 변경

--- core/core-api/src/main/java/com/winten/greenlight/core/api/controller/v1/MemberController.java
+++ core/core-api/src/main/java/com/winten/greenlight/core/api/controller/v1/MemberController.java
@@ -19,9 +19,12 @@ public class MemberController {
     @GetMapping("/{memberId}/queue")
     public Mono<ApiResponse<MemberQueueResponseDto>> getMemberQueueInfo(@PathVariable String memberId) {
         //eventId: parameter로 전달 받자.
+        //GET /members/{memberId}/queue로 RESTful하게 개선.
 
         return memberService.getMemberQueueInfo(new Customer("1234", memberId, -1, WaitingStatus.READY))
             .map(MemberQueueResponseDto::new) // MemberQueueResponseDto로 변환
             .map(ApiResponse::success);
     }
+
+    //http://localhost:8080/v1/members/0JENEC2Y643A7/queue
 }
--- domain/src/main/java/com/winten/greenlight/domain/MemberService.java
+++ domain/src/main/java/com/winten/greenlight/domain/MemberService.java
@@ -12,16 +12,23 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     // 대기 정보 조회
-    // 대기 정보 조회
     public Mono<MemberQueueResponse> getMemberQueueInfo(Customer customer) {
         return memberRepository.getMemberQueuePosition(customer)
             .zipWith(memberRepository.getTotalWaitingCount(customer))
             .map(tuple -> { //결과를 tuple로 묶어 하나의 Mono로 반환.
-                int position = tuple.getT1();
-                int total = tuple.getT2();
-                int remaining = total - position;
+                int queueNumber = tuple.getT1(); // 현재 고객의 대기 순번
+                int total = tuple.getT2(); // 전체 대기자 수
+                int remaining;
+
+                if (queueNumber == -1) {
+                    queueNumber = 0; // 대기열에 없으면 0으로 보정
+                    remaining = total; // 전체 대기자 수 그대로 사용
+                } else {
+                    remaining = total - queueNumber;
+                }
+
                 int estimatedTime = estimateWaitTime(remaining);
-                return new MemberQueueResponse(position, remaining, estimatedTime);
+                return new MemberQueueResponse(queueNumber, remaining, estimatedTime);
             });
     }
 
